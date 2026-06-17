8e30f4653a0e211231bd7d729524810ba50f5c7b
SharonIV0X86 <anirudhlakhanpal123@gmail.com>
2026-06-18 01:35:57 +0530

    fix: fixed non-existent modules

---
+++ himvirasat-frontend/app/admin/dashboard/contributions/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import { secondaryButtonStyles } from "@/lib/constants";
+
+export default function ContributionsPage() {
+    return (
+        <div>
+            <Button className={secondaryButtonStyles}>
+                Test
+            </Button>
+        </div>
+    );
+}
---
+++ himvirasat-frontend/app/admin/dashboard/datasets/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import { secondaryButtonStyles } from "@/lib/constants";
+
+export default function DatasetsPage() {
+    return (
+        <div>
+            <Button className={secondaryButtonStyles}>
+                Test
+            </Button>
+        </div>
+    );
+}

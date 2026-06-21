3d5a5de8cec4921689f87115409a374930178097
Chaitanya Tyagi <casesilver777@gmail.com>
2026-06-22 01:48:31 +0530

    Update server.ts

--- himvirasat-backend/src/server.ts
+++ himvirasat-backend/src/server.ts
@@ -2,5 +2,8 @@ import { app } from "./app.js";
 import { env } from "./config/env.js";
 
 app.listen(env.PORT, () => {
+  console.log("PORT =", process.env.PORT);
+  console.log("JWT_SECRET exists =", !!process.env.JWT_SECRET);
+  console.log("SUPABASE_URL exists =", !!process.env.SUPABASE_URL);
   console.log(`Server running on port ${env.PORT}`);
 });
